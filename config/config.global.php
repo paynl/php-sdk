4e247f80f2ca84f0456efcb8b54f450814295e56
woutse <wouter@pay.nl>
2024-11-13 13:25:20 +0100

    Updates for v3

--- config/config.global.php
+++ config/config.global.php
@@ -4,11 +4,9 @@ declare(strict_types=1);
 
 return [
     'authentication' => [
-        #'username' => 'AT-0045-3283', # Use AT-Code or SL-Code. Use AT-code together with API-Token.
-        #'password' => 'a3e314eef828b10d53eb9c1142b00f0262c14ab2', # Use API-Token or secret. Use Secret in combination with SL-Code.
+        'username' => '', # Use AT-Code or SL-Code. Use AT-code together with API-Token.
+        'password' => '', # Use API-Token or secret. Use Secret in combination with SL-Code.
     ],
     'debug' => false,
-    #'failoverUrl' => 'https://dev-wouter.tintel.dev/sandbox_sdk3.php',
-    #'api' => ['url' => '']  # Core
     #'api' => ['url' => '']  # Core
 ];
